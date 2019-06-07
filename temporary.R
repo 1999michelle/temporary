195bd05651ae150f5e277d6bc4521e9d895f8939
Yang <12001003748416@fedidcard.gov>
2019-06-07 13:55:23 -0700

    minor changes

---
+++ temporary.R
@@ -0,0 +1,7 @@
+# == Program Name: temporary.R ========================================
+
+# This is just an example. Setting up GitHub Account
+
+rename <- sample(1:5, 5, replace = FALSE)
+# [1] 5 1 2 3 4
+
